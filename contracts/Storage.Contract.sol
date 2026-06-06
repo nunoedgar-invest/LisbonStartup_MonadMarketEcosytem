e05b1d6eb84263b893a882f14458bd7dfacd1b3c
Nuno Edgar Nunes Fernandes <56157677+nunoedgar-invest@users.noreply.github.com>
2026-06-06 15:41:50 +0100

    Implement storage contract with item management

--- contracts/Storage.Contract.sol
+++ contracts/Storage.Contract.sol
@@ -1,3 +1,17 @@
+    StorageItem[] private storageItems;
+    address public owner;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function storeItem(uint256 _id, string memory _key, string memory _value) public {
+        storageItems.push(StorageItem(_id, _key, _value));
+    }
+    function getItem(uint256 _id) public view returns (string memory) {
+        require(_id < storageItems.length, "ID out of range");
+        return storageItems[_id].value;
+    }
+}
 
 
